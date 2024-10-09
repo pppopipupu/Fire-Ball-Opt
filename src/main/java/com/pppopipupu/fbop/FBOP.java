08127c5bc54d272d62e9c8b9b55d68a6457029ea
pppopipupu <pppopipupu@gamil.com>
2024-10-09 21:10:20 +0800

    update

--- src/main/java/com/pppopipupu/fbop/FBOP.java
+++ src/main/java/com/pppopipupu/fbop/FBOP.java
@@ -1,13 +1,12 @@
-package com.example.modid;
+package com.pppopipupu.fbop;
 
-import com.example.modid.Tags;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
-public class ExampleMod {
+public class FBOP {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 
@@ -18,7 +17,7 @@ public class ExampleMod {
      */
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        LOGGER.info("Hello From {}!", Tags.MOD_NAME);
+        LOGGER.info("FBOP Pre-Initialization");
     }
 
 }
---
+++ src/main/java/com/pppopipupu/fbop/mixin/MixinItemFireBall.java
@@ -0,0 +1,26 @@
+package com.pppopipupu.fbop.mixin;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemFireball;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(ItemFireball.class)
+public class MixinItemFireBall {
+    @Overwrite
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+
+          return EnumActionResult.PASS;
+
+    }
+}
